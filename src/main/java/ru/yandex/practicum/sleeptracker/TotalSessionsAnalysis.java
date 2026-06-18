471e225914d6ce9b0b657d00a51ad7d7547f4203
Nikita768 <--list>
2026-06-18 11:05:29 +0300

    Закончить проект

--- src/main/java/ru/yandex/practicum/sleeptracker/TotalSessionsAnalysis.java
+++ src/main/java/ru/yandex/practicum/sleeptracker/TotalSessionsAnalysis.java
@@ -5,7 +5,7 @@ import java.util.function.Function;
 
 public class TotalSessionsAnalysis implements Function<List<SleepingSession>, SleepAnaLysisResult<Integer>> {
     @Override
-    public SleepAnaLysisResult<Integer> apply (List<SleepingSession> records) {
+    public SleepAnaLysisResult<Integer> apply(List<SleepingSession> records) {
         return new SleepAnaLysisResult("Количество сессий сна: ", records.size());
     }
 }
